bd00372c11e56210edbc69debd09e93e6743ab1f
Christy O'Reilly <christyor73@gmail.com>
2011-11-02 21:12:24 +0000

    Initial commit

---
+++ src/miso/core.clj
@@ -0,0 +1,1 @@
+(ns miso.core)
---
+++ test/miso/test/core.clj
@@ -0,0 +1,6 @@
+(ns miso.test.core
+  (:use [miso.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
