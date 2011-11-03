a69900f7a8baf59f6bf25fadd0835f722f0f6968
Christy O'Reilly <christyor73@gmail.com>
2011-11-03 08:53:27 +0000

    Testy stuff

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject miso "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [server-socket "1.0.0"]]
+  :main miso.core)
--- src/miso/core.clj
+++ src/miso/core.clj
@@ -1,1 +1,28 @@
 (ns miso.core)
+
+(use 'clojure.java.io)
+(use 'server.socket)
+
+(defn move [dir]
+  (println (format "moving %swards" dir)))
+
+(defn serv-loop [input]
+  (cond
+    (and (empty? (rest input))
+         (some #{(first input)} "12346789")) (move (first input)),
+    true (println (format "wtf is '%s'?" input)),
+    ))
+
+(defn game-serv []
+  (letfn
+    [(game [in out]
+                (binding [*in* (reader in)
+                          *out* (writer out)]
+                  (loop [input (read-line)]
+                    (serv-loop input)
+                    (if (= input "exit")
+                      (println "Byeeee")
+                      (recur (read-line))))))]
+    (create-server 8080 game)))
+
+(def my-server (game-serv))
