2d5f25f3a4ce023af3e21f52097bc211f12c0fed
Christy O'Reilly <christyor73@gmail.com>
2011-11-04 08:48:59 +0000

    More tests

--- src/miso/core.clj
+++ src/miso/core.clj
@@ -2,6 +2,7 @@
 
 (use 'clojure.java.io)
 (use 'server.socket)
+(use '[clojure.string :only (join)])
 
 (defn move [dir]
   (println (format "moving %swards" dir)))
@@ -16,13 +17,51 @@
 (defn game-serv []
   (letfn
     [(game [in out]
-                (binding [*in* (reader in)
-                          *out* (writer out)]
-                  (loop [input (read-line)]
-                    (serv-loop input)
-                    (if (= input "exit")
-                      (println "Byeeee")
-                      (recur (read-line))))))]
+           (binding [*in* (reader in)
+                     *out* (writer out)]
+             (loop [input (read-line)]
+               (serv-loop input)
+               (if (= input "exit")
+                 (println "Byeeee")
+                 (recur (read-line))))))]
     (create-server 8080 game)))
 
 (def my-server (game-serv))
+
+(def make-vector (comp vec repeat))
+
+(defn make-room [x y]
+  (hash-map
+    ; Rooms are vertically stacked vectors of horizontal rows
+    :space (make-vector y
+                        (make-vector x \.)),
+    :players (),
+    ))
+
+(defn get-cell [room x y]
+  (get-in (room :space) [y x]))
+
+(defn print-room [room]
+  (print
+    (join "\n"
+          (for [row (room :space)]
+            (join " " row)))))
+
+;(def *rooms* (hash-map
+(def rooms (hash-map
+             :start (make-room 10 6),
+             :next (make-room 5 5),))
+
+;(def *players* (hash-map))
+
+(defn make-player [name]
+  (let [start-room (rooms :start)]
+    (assoc start-room :players
+           (conj (start-room :players)
+                 (hash-map
+                   :name name,
+                   :x 0,
+                   :y 0,
+                   :room :start,)))))
+
+(make-player "bob")
