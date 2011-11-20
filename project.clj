d2e0838553d28b5e602c20a21417ed6d84f46526
Christy O'Reilly <christyor73@gmail.com>
2011-11-20 22:42:00 +0000

    Add nailgun

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [server-socket "1.0.0"]]
+  :dev-dependencies [[org.clojars.ibdknox/lein-nailgun "1.1.1"]]
   :main miso.core)
