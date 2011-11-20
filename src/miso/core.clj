77826e3ed508c97ce4fa063b53b0ce9b82e00e97
Christy O'Reilly <christyor73@gmail.com>
2011-11-20 18:16:25 +0000

    WIP

--- src/miso/core.clj
+++ src/miso/core.clj
@@ -4,55 +4,33 @@
 (use 'server.socket)
 (use '[clojure.string :only (join)])
 
+(def ^:dynamic *current-room*)
+
 (defn move [dir]
   (println (format "moving %swards" dir)))
-
-(defn serv-loop [input]
-  (cond
-    (and (empty? (rest input))
-         (some #{(first input)} "12346789")) (move (first input)),
-    true (println (format "wtf is '%s'?" input)),
-    ))
-
-(defn game-serv []
-  (letfn
-    [(game [in out]
-           (binding [*in* (reader in)
-                     *out* (writer out)]
-             (loop [input (read-line)]
-               (serv-loop input)
-               (if (= input "exit")
-                 (println "Byeeee")
-                 (recur (read-line))))))]
-    (create-server 8080 game)))
-
-(def my-server (game-serv))
 
 (def make-vector (comp vec repeat))
 
 (defn make-room [x y]
   (hash-map
     ; Rooms are vertically stacked vectors of horizontal rows
-    :space (make-vector y
-                        (make-vector x \.)),
-    :players (),
+    :space (ref (make-vector y
+                        (make-vector x \.))),
+    :players (ref ()),
     ))
 
 (defn get-cell [room x y]
   (get-in (room :space) [y x]))
 
 (defn print-room [room]
-  (print
+  (println
     (join "\n"
           (for [row (room :space)]
             (join " " row)))))
 
-;(def *rooms* (hash-map
-(def rooms (hash-map
-             :start (make-room 10 6),
-             :next (make-room 5 5),))
-
-;(def *players* (hash-map))
+(def rooms (ref (hash-map
+                  :start (make-room 10 6),
+                  :next (make-room 5 5),)))
 
 (defn make-player [name]
   (let [start-room (rooms :start)]
@@ -62,6 +40,29 @@
                    :name name,
                    :x 0,
                    :y 0,
-                   :room :start,)))))
+                   )))))
 
 (make-player "bob")
+
+(defn client-input-loop [input]
+  (cond
+    (and (empty? (rest input))
+         (some #{(first input)} "12346789")) (move (first input)),
+    true (println (format "wtf is '%s'?" input)),
+    ))
+
+(defn game-handler [in out]
+  (binding [*in* (reader in)
+            *out* (writer out)]
+    (binding [*current-room* (ref (rooms :start))]
+      (loop [input (read-line)]
+        (client-input-loop input)
+        (print-room *current-room*)
+        (if (= input "exit")
+          (println "Byeeee")
+          (recur (read-line)))))))
+
+(defn game-serv []
+  (create-server 8080 game-handler))
+
+(def my-server (game-serv))
